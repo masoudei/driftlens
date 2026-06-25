647bf8e1dbef6f9bd87bb5c3d4f38874b362430b
Masoud Ehteshami <ehteshami.developer@gmail.com>
2026-06-25 03:45:05 +0300

    feat: mock collector now emits varying replicas/image to trigger drift detection

--- internal/collector/mock/collector.go
+++ internal/collector/mock/collector.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	"math/rand"
+	"sync"
 	"time"
 
 	"github.com/masoudei/driftlens/internal/eventbus"
@@ -13,20 +14,25 @@ var resources = []string{"Deployment", "StatefulSet", "DaemonSet", "ConfigMap", 
 var names = []string{"payment-api", "checkout-api", "users-svc", "orders-svc", "inventory-svc"}
 
 type Collector struct {
-	bus    eventbus.Bus
-	cancel context.CancelFunc
-	events int
+	bus       eventbus.Bus
+	cancel    context.CancelFunc
+	events    int
+	replicas  map[string]int
+	replicasMu sync.Mutex
 }
 
 func New(bus eventbus.Bus) *Collector {
-	return &Collector{bus: bus}
+	return &Collector{
+		bus:      bus,
+		replicas: make(map[string]int),
+	}
 }
 
 func (c *Collector) Start(ctx context.Context) error {
 	ctx, c.cancel = context.WithCancel(ctx)
 
 	for i, r := range resources {
-		c.emit(ctx, r, names[i], eventbus.EventResourceCreated)
+		c.emit(ctx, r, names[i], eventbus.EventResourceCreated, 3)
 	}
 
 	go func() {
@@ -39,12 +45,12 @@ func (c *Collector) Start(ctx context.Context) error {
 			case <-ticker.C:
 				r := resources[rand.Intn(len(resources))]
 				n := names[rand.Intn(len(names))]
-				types := []eventbus.EventType{
-					eventbus.EventResourceUpdated,
-					eventbus.EventResourceUpdated,
-					eventbus.EventResourceDeleted,
+
+				if rand.Intn(3) == 0 {
+					c.emit(ctx, r, n, eventbus.EventResourceDeleted, 0)
+				} else {
+					c.emit(ctx, r, n, eventbus.EventResourceUpdated, rand.Intn(10)+1)
 				}
-				c.emit(ctx, r, n, types[rand.Intn(len(types))])
 			}
 		}
 	}()
@@ -58,18 +64,44 @@ func (c *Collector) Stop() {
 	}
 }
 
-func (c *Collector) emit(ctx context.Context, resource, name string, eventType eventbus.EventType) {
+func (c *Collector) emit(ctx context.Context, resource, name string, eventType eventbus.EventType, replicas int) {
 	id := fmt.Sprintf("%s/%s/%s", "default", resource, name)
+
+	c.replicasMu.Lock()
+	prevReplicas := c.replicas[id]
+	if eventType == eventbus.EventResourceCreated || eventType == eventbus.EventResourceUpdated {
+		c.replicas[id] = replicas
+	} else if eventType == eventbus.EventResourceDeleted {
+		delete(c.replicas, id)
+	}
+	c.replicasMu.Unlock()
+
+	data := map[string]any{
+		"resource":  resource,
+		"name":      name,
+		"namespace": "default",
+		"id":        id,
+	}
+
+	if resource == "Deployment" || resource == "StatefulSet" || resource == "DaemonSet" {
+		data["replicas"] = fmt.Sprintf("%d", replicas)
+		images := []string{"nginx:1.25", "nginx:1.26", "nginx:1.27", "app:v2", "app:v3"}
+		data["image"] = images[replicas%len(images)]
+	}
+
+	if resource == "ConfigMap" {
+		data["data_keys"] = fmt.Sprintf("config-%d", replicas)
+	}
+
+	if eventType == eventbus.EventResourceUpdated {
+		data["prev_replicas"] = fmt.Sprintf("%d", prevReplicas)
+	}
+
 	c.bus.Publish(eventbus.Event{
 		Type:      eventType,
 		Source:    "mock-collector",
 		Timestamp: time.Now().UTC(),
-		Data: map[string]any{
-			"resource":  resource,
-			"name":      name,
-			"namespace": "default",
-			"id":        id,
-		},
+		Data:      data,
 	})
 	c.events++
 }
